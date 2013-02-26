8f8219d3df4dd91f30b7d5dd44c58f191a71f6e1
Aura Software <support@aura-software.com>
2013-02-26 20:30:16 +0000

    move into subdir

---
+++ comment_excluder/comment_excluder.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: Members: Exclude roles from commenting
+Plugin URI: http://themehybrid.com/support/topic/approve-comments-for-specified-roles-only?replies=9#post-76992
+Description: First create the capability add_comment then add it to the roles that should be able to comment. Finally create a new role whithout this capability.
+Author: Justin Tadlock
+Version: 1.0
+Author URI: http://themehybrid.com/
+*/
+add_filter( 'comments_open', 'my_comments_open' );
+
+function my_comments_open( $open ) {
+	return current_user_can( 'add_comment' ) ? $open : false;
+}
+?>
